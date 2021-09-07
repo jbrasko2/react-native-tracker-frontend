75886d108426ea2ab5ca4684cca7ef2491607616
jbrasko <jbrasko0508@gmail.com>
2021-09-07 16:55:53 -0400

    fix return statement

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -12,7 +12,7 @@ export default (reducer, actions, defaultValue) => {
     }
 
     return (
-      <Context.Provider value={{ state, boundActions }}>
+      <Context.Provider value={{ state, ...boundActions }}>
         {children}
       </Context.Provider>
     )
