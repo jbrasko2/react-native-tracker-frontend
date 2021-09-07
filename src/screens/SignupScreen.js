a57061d08f666ac9239690c4eba8566805f678ae
jbrasko <jbrasko0508@gmail.com>
2021-09-07 14:34:34 -0400

    create and add Spacer component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
+import Spacer from './components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
   return (
     <>
-      <Text h3>Sign Up for Tracker</Text>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
       <Input label='Email' />
+      <Spacer />
       <Input label='Password' />
-      <Button title='Sign Up' />
+      <Spacer>
+        <Button title='Sign Up' />
+      </Spacer>
     </>
   )
 }
