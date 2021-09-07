5a46786c1c754fd15b025bab2296479ae87c7a36
jbrasko <jbrasko0508@gmail.com>
2021-09-07 14:51:06 -0400

    move components folder

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
-import Spacer from './components/Spacer'
+import Spacer from '../components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
   const [email, setEmail] = useState('')
