11efd6eaabbef21e7a2966243fc59e3e5f684c21
jbrasko <jbrasko0508@gmail.com>
2021-09-09 12:18:02 -0400

    add link to sign in instead;

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, TouchableOpacity } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
 import { Context as AuthContext } from '../context/AuthContext'
@@ -36,6 +36,13 @@ const SignupScreen = ({ navigation }) => {
       <Spacer>
         <Button title='Sign Up' onPress={() => signup({ email, password })} />
       </Spacer>
+      <Spacer>
+        <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+          <Text style={styles.link}>
+            Already have an account? Sign in instead
+          </Text>
+        </TouchableOpacity>
+      </Spacer>
     </View>
   )
 }
@@ -56,6 +63,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: 'red',
     marginLeft: 15,
+  },
+  link: {
+    color: 'blue',
   },
 })
 
