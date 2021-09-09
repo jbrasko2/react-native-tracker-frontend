a1b6fa5a5beb8fb8009dbb4f8cf77afd924a7668
jbrasko <jbrasko0508@gmail.com>
2021-09-09 12:12:43 -0400

    navigate to TrackList after signup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { navigate } from '../navigationRef'
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -22,6 +23,8 @@ const signup = dispatch => {
       const response = await trackerApi.post('/signup', { email, password })
       await AsyncStorage.setItem('token', response.data.token)
       dispatch({ type: 'signup', payload: response.data.token })
+
+      navigate('TrackList')
     } catch (err) {
       dispatch({
         type: 'add_error',
