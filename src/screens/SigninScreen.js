0d6e6c0da0df87c42a1da597c0140f910da3be3d
jbrasko <jbrasko0508@gmail.com>
2021-09-09 12:38:09 -0400

    build SigninScreen from SignupScreen foundation

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useContext } from 'react'
+import { StyleSheet, View, TouchableOpacity } from 'react-native'
+import { Text, Input, Button } from 'react-native-elements'
+import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
 
-const SigninScreen = () => {
+const SigninScreen = ({ navigation }) => {
+  const { state, signin } = useContext(AuthContext)
+
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText='Sign In for Tracker'
+        submitButtonText='Sign In'
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+      />
+      <Spacer>
+        <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+          <Text style={styles.link}>
+            Dont' have an account? Sign up instead
+          </Text>
+        </TouchableOpacity>
+      </Spacer>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200,
+  },
+  link: {
+    color: 'blue',
+  },
+})
 
 export default SigninScreen
