4d95efb51f6544800c6b8b821f8565419e1cefc8
jbrasko <jbrasko0508@gmail.com>
2021-09-13 12:54:16 -0400

    add signout functionality

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button } from 'react-native-elements'
+import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext'
 
 const AccountScreen = () => {
+  const { signout } = useContext(AuthContext)
+
   return (
     <View>
       <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Spacer>
+        <Button title='Sign Out' onPress={signout} />
+      </Spacer>
     </View>
   )
 }
