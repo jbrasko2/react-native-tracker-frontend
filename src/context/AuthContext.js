d4a19ff074833a3548ee128c4f96fbe95a96008f
jbrasko <jbrasko0508@gmail.com>
2021-09-13 11:58:21 -0400

    export tryLocalSigin

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -80,6 +80,6 @@ const signout = dispatch => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup, clearErrorMessage },
+  { signin, signout, signup, clearErrorMessage, tryLocalSignin },
   { token: null, errorMessage: '' }
 )
