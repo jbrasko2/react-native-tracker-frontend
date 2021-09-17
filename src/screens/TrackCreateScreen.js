571b9552622f65695df509709e426bc72e5cbbe5
jbrasko <jbrasko0508@gmail.com>
2021-09-17 15:41:44 -0400

    add withNavigationFocus

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,11 +3,12 @@ import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import Map from '../components/Map'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { withNavigationFocus } from 'react-navigation'
 import '../_mockLocation'
 import { Context as LocationContext } from '../context/LocationContext'
 import useLocation from '../hooks/useLocation'
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext)
 
   const [err] = useLocation(addLocation)
@@ -23,4 +24,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({})
 
-export default TrackCreateScreen
+export default withNavigationFocus(TrackCreateScreen)
