c2389eb527deb2afd80fe15e419fcf47ad4188b7
jbrasko <jbrasko0508@gmail.com>
2021-09-17 14:39:32 -0400

    fix type in LocationProvider import

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackListScreen from './src/screens/TrackListScreen'
 import ResolveAuthScreen from './src/screens/ResolveAuthScreen'
 import { Provider as AuthProvider } from './src/context/AuthContext'
-import { Provider as LocationProvider } from './src/LocationContext/'
+import { Provider as LocationProvider } from './src/context/LocationContext/'
 import { setNavigator } from './src/navigationRef'
 
 const switchNavigator = createSwitchNavigator({
