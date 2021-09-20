418a897b5325fb09fb7ee65e929e5600c2edb32d
jbrasko <jbrasko0508@gmail.com>
2021-09-20 13:14:47 -0400

    add cleanup function to useEffect

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -38,6 +38,12 @@ export default (shouldTrack, callback) => {
       subscriber.remove()
       setSubscriber(null)
     }
+
+    return () => {
+      if (subscriber) {
+        subscriber.remove()
+      }
+    }
   }, [shouldTrack, callback])
 
   return [err]
