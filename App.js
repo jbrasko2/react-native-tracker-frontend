206e22f5df8d375a8b6848870db1c7281150febe
jbrasko <jbrasko0508@gmail.com>
2021-09-20 15:24:20 -0400

    fix typo in import

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import TrackListScreen from './src/screens/TrackListScreen'
 import ResolveAuthScreen from './src/screens/ResolveAuthScreen'
 import { Provider as AuthProvider } from './src/context/AuthContext'
 import { Provider as LocationProvider } from './src/context/LocationContext/'
-import { Provder as TrackProvuder } from './src/context/TrackContext'
+import { Provder as TrackProvider } from './src/context/TrackContext'
 import { setNavigator } from './src/navigationRef'
 
 const switchNavigator = createSwitchNavigator({
