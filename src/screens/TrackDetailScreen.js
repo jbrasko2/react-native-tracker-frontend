80045f1c5cdda54b0258912dc401012fbfbee29a
jbrasko <jbrasko0508@gmail.com>
2021-09-20 16:47:04 -0400

    start TrackDetailScreen details

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Context as TrackContext } from '../context/TrackContext'
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext)
+  const _id = navigation.getParam('_id')
+
+  const track = state.find(t => t._id === _id)
+
   return (
     <View>
-      <Text style={{ fontSize: 48 }}>TrackDetailScreen</Text>
+      <Text style={{ fontSize: 48 }}>{track.name}</Text>
     </View>
   )
 }
