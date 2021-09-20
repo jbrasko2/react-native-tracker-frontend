d2fa54971d0e21588b71a5b7b63a4bb26ca71d0d
jbrasko <jbrasko0508@gmail.com>
2021-09-20 15:12:47 -0400

    add Polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,7 @@ import { Context as LocationContext } from '../context/LocationContext'
 
 const Map = () => {
   const {
-    state: { currentLocation },
+    state: { currentLocation, locations },
   } = useContext(LocationContext)
 
   if (!currentLocation) {
@@ -21,17 +21,18 @@ const Map = () => {
         longitudeDelta: 0.01,
       }}
       // region={{
-        // ...currentLocation.coords,
-        // latitudeDelta: 0.01,
-        // longitudeDelta: 0.01,
+      // ...currentLocation.coords,
+      // latitudeDelta: 0.01,
+      // longitudeDelta: 0.01,
       // }}
     >
-      <Circle 
+      <Circle
         center={currentLocation.coords}
         radius={30}
         strokeColor='rgba(158, 158, 255, 1.0)'
         fillColor='rgba(158, 158, 255, 0.3)'
       />
+      <Polyline coordinates={locations.map(loc => loc.coords)} />
     </MapView>
   )
 }
