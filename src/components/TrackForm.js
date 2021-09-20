23628a5a1f95f20a54050a24ed516c5fa28d5046
jbrasko <jbrasko0508@gmail.com>
2021-09-20 10:29:34 -0400

    create TrackForm

---
+++ src/components/TrackForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Input, Button } from 'react-native-elements'
+
+const TrackForm = () => {
+  return (
+    <>
+      <Input />
+      <Button title='Start Recording' />
+    </>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default TrackForm
