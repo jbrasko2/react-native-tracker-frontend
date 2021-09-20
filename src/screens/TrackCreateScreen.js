07a2f1fb6b6ba52533c97ac222cf643f9307509a
jbrasko <jbrasko0508@gmail.com>
2021-09-20 15:06:09 -0400

    destructure recording

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -10,12 +10,12 @@ import useLocation from '../hooks/useLocation'
 import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { state, addLocation } = useContext(LocationContext)
+  const { state: { recording }, addLocation } = useContext(LocationContext)
   const callback = useCallback(
-    location => addLocation(location, state.recording),
-    [state.recording]
+    location => addLocation(location, recording),
+    [recording]
   )
-  const [err] = useLocation(isFocused || state.recording, callback)
+  const [err] = useLocation(isFocused || recording, callback)
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
