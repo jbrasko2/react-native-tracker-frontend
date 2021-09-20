766198a1de9853ad4def59e3e9e42e4f5bac4000
jbrasko <jbrasko0508@gmail.com>
2021-09-20 16:07:06 -0400

    reset form after track save

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -12,6 +12,8 @@ const locationReducer = (state, action) => {
       return { ...state, locations: [...state.locations, action.payload] }
     case 'change_name':
       return { ...state, name: action.payload }
+    case 'reset':
+      return { ...state, name: '', locations: [] }
     default:
       return state
   }
@@ -36,8 +38,12 @@ const changeName = dispatch => name => {
   dispatch({ type: 'change_name', payload: name })
 }
 
+const reset = dispatch => () => {
+  dispatch({ type: 'reset' })
+}
+
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation, changeName },
+  { startRecording, stopRecording, addLocation, changeName, reset },
   { name: '', recording: false, locations: [], currentLocation: null }
 )
--- src/hooks/useSaveTrack.js
+++ src/hooks/useSaveTrack.js
@@ -6,10 +6,12 @@ export default () => {
   const { createTrack } = useContext(TrackContext)
   const {
     state: { locations, name },
+    reset,
   } = useContext(LocationContext)
 
-  const saveTrack = () => {
-    createTrack(name, locations)
+  const saveTrack = async () => {
+    await createTrack(name, locations)
+    reset()
   }
 
   return [saveTrack]
