645d332aebff01e0f05ad89686333ed1dba3f66a
jbrasko <jbrasko0508@gmail.com>
2021-09-20 13:26:30 -0400

    remove subscriber from state, set as local variable. move startWatching into useEffect

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,36 +7,38 @@ import {
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null)
-  const [subscriber, setSubscriber] = useState(null)
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync()
-
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distance: 10,
-        },
-        callback
-      )
-      setSubscriber(sub)
-
-      if (!granted) {
-        throw new Error('Location permission not granted')
-      }
-    } catch (e) {
-      setErr(e)
-    }
-  }
 
   useEffect(() => {
+    let subscriber
+
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestForegroundPermissionsAsync()
+
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distance: 10,
+          },
+          callback
+        )
+
+        if (!granted) {
+          throw new Error('Location permission not granted')
+        }
+      } catch (e) {
+        setErr(e)
+      }
+    }
+
     if (shouldTrack) {
       startWatching()
     } else {
-      subscriber.remove()
-      setSubscriber(null)
+      if (subscriber) {
+        subscriber.remove()
+      }
+      subscriber = null
     }
 
     return () => {
