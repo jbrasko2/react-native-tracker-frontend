07714691ab5bbb649e0a989607aaf16036af3dd8
jbrasko <jbrasko0508@gmail.com>
2021-09-20 16:58:16 -0400

    add header, remove title text

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -10,7 +10,6 @@ const TrackListScreen = ({ navigation }) => {
   return (
     <>
       <NavigationEvents onWillFocus={fetchTracks} />
-      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
       <FlatList
         data={state}
         keyExtractor={item => item._id}
@@ -33,6 +32,10 @@ const TrackListScreen = ({ navigation }) => {
       />
     </>
   )
+}
+
+TrackListScreen.navigationOptions = {
+  title: 'Tracks',
 }
 
 const styles = StyleSheet.create({})
