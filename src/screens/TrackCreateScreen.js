e3785da25c1d80a9d15b4bceb5b1eb89980664c1
jbrasko <jbrasko0508@gmail.com>
2021-09-20 10:46:48 -0400

    pass in recording as argument for addLocation

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -10,9 +10,11 @@ import useLocation from '../hooks/useLocation'
 import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext)
+  const { state, addLocation } = useContext(LocationContext)
 
-  const [err] = useLocation(isFocused, addLocation)
+  const [err] = useLocation(isFocused, location => {
+    addLocation(location, state.recording)
+  })
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
