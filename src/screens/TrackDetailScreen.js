bfa1c84cf5a0509a2e28a960366dc2c0bc8b7487
jbrasko <jbrasko0508@gmail.com>
2021-09-20 16:52:05 -0400

    add MapView

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,20 +1,36 @@
 import React, { useContext } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Context as TrackContext } from '../context/TrackContext'
+import MapView, { Polygon, Polyline } from 'react-native-maps'
 
 const TrackDetailScreen = ({ navigation }) => {
   const { state } = useContext(TrackContext)
   const _id = navigation.getParam('_id')
 
   const track = state.find(t => t._id === _id)
+  const initialCoords = track.locations[0].coords
 
   return (
-    <View>
+    <>
       <Text style={{ fontSize: 48 }}>{track.name}</Text>
-    </View>
+      <MapView
+        initialRegion={{
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+          ...initialCoords,
+        }}
+        style={styles.map}
+      >
+        <Polyline coordinates={track.locations.map(loc => loc.coords)} />
+      </MapView>
+    </>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+})
 
 export default TrackDetailScreen
