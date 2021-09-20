4d10dba387f358167fa97b505e5991e6b7287886
jbrasko <jbrasko0508@gmail.com>
2021-09-20 12:25:44 -0400

    add functions, context to TrackForm

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 import Spacer from './Spacer'
+import { Context as LocationContext } from '../context/LocationContext'
 
 const TrackForm = () => {
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
+  } = useContext(LocationContext)
+
+  console.log(locations.length)
+
   return (
     <>
       <Spacer>
-        <Input placeholder='Enter Track Name' />
+        <Input
+          value={name}
+          onChangeText={changeName}
+          placeholder='Enter Track Name'
+        />
       </Spacer>
-      <Button title='Start Recording' />
+      {recording ? (
+        <Button title='Stop' onPress={stopRecording} />
+      ) : (
+        <Button title='Start Recording' onPress={startRecording} />
+      )}
     </>
   )
 }
