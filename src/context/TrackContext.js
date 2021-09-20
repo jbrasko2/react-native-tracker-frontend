0ef7729b7a6f3a0d29180a951d5dc618bd43ff69
jbrasko <jbrasko0508@gmail.com>
2021-09-20 16:04:20 -0400

    fix action typo

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -1,7 +1,7 @@
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
 
-const trackReducer = (state, actions) => {
+const trackReducer = (state, action) => {
   switch (action.type) {
     default:
       return state
